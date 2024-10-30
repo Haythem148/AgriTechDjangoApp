459d781ed907a14d5e24777ded3f8af985f10829
wnaggui <wadhah.naggui@esprit.tn>
2024-10-30 10:42:07 +0100

    commit

--- dechets/data/genrate.py
+++ dechets/data/genrate.py
@@ -4,13 +4,13 @@ import numpy as np
 # Génération de données aléatoires
 np.random.seed(42)
 data = {
-    "TypeDechet": np.random.choice(["Déchets verts", "Plastique", "Papier", "Métal", "Déchets ménagers", "Déchets de bois", "Carton", "Textiles", "Déchets alimentaires", "Verre", "Déchets organiques", "Plastique dur"], 500),
-    "Quantite": np.random.randint(10, 200, size=500),
-    "Biodegradable": np.random.choice([0, 1], size=500),
-    "Method": np.random.choice(["Compostage", "Recyclage", "Incinération", "Enfouissement"], 500),
-    "Temperature": np.random.uniform(15, 850, size=500),
-    "Humidity": np.random.uniform(10, 80, size=500),
-    "Efficacite": np.random.uniform(40, 90, size=500)
+    "TypeDechet": np.random.choice(["Déchets verts", "Plastique", "Papier", "Métal", "Déchets ménagers", "Déchets de bois", "Carton", "Textiles", "Déchets alimentaires", "Verre", "Déchets organiques", "Plastique dur"], 3000),
+    "Quantite": np.random.randint(10, 200, size=3000),
+    "Biodegradable": np.random.choice([0, 1], size=3000),
+    "Method": np.random.choice(["Compostage", "Recyclage", "Incinération", "Enfouissement"], 3000),
+    "Temperature": np.random.uniform(15, 850, size=3000),
+    "Humidity": np.random.uniform(10, 80, size=3000),
+    "Efficacite": np.random.uniform(40, 90, size=3000)
 }
 
 # Création du DataFrame et sauvegarde en CSV
