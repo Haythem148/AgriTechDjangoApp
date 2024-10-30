a6b38237b6a6d94e57ebbf5a1f173966dc53c0a0
Haythem Mekki <149381807+Haythem148@users.noreply.github.com>
2024-10-30 09:39:35 +0100

    Create middleware.py

---
+++ dashboard/middleware.py
@@ -0,0 +1,18 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+
+class AuthenticationMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # List of URLs that don't require authentication
+        exempt_urls = [
+            reverse('dashboard:login'),
+        ]
+
+        if not request.user.is_authenticated and request.path not in exempt_urls:
+            return redirect('dashboard:login')
+
+        response = self.get_response(request)
+        return response
